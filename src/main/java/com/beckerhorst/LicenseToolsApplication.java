179544221a355db18adc85af1067b381bc266d39
Dirk Becker-Horst <dirk@becker-horst.com>
2016-11-20 12:39:32 +0100

    Add demo license

--- src/main/java/com/beckerhorst/LicenseToolsApplication.java
+++ src/main/java/com/beckerhorst/LicenseToolsApplication.java
@@ -1,7 +1,14 @@
 package com.beckerhorst;
 
+import java.util.GregorianCalendar;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.beckerhorst.db.License;
+import com.beckerhorst.db.LicenseRepository;
 
 /**
  * Main application with some demo licenses for the license tool.
@@ -10,6 +17,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class LicenseToolsApplication {
+	
+	@Bean
+	public CommandLineRunner loadData(LicenseRepository repository) {
+		return (args) -> {
+			// save a couple of licenses
+			License newLicense = new License("License Tool", new GregorianCalendar());
+			newLicense.setExpirationDate(new GregorianCalendar(2020, 01, 01));
+			repository.save(newLicense);
+		};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LicenseToolsApplication.class, args);
 	}
